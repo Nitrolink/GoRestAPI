dad50f6e8e7d37aac58e9c0741fbbfee736f1520
Conor <driscollconor3@gmail.com>
2021-11-18 17:09:51 -0500

    Initial commit

---
+++ src/main/java/com/Foundry/GoRestAPI/GoRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.Foundry.GoRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Foundry/GoRestAPI/GoRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Foundry.GoRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
